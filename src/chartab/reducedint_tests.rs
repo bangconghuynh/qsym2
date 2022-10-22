aa875d1b8c9664d2f46478e6524e60449afe558c
Bang C. Huynh <bang.cong.huynh@gmail.com>
2022-10-22 21:54:32 +0100

    Added more reducedint tests with arrays.

--- src/chartab/reducedint_tests.rs
+++ src/chartab/reducedint_tests.rs
@@ -1,6 +1,8 @@
 use std::panic;
 
+use num_traits::Inv;
 use num_modular::{ModularInteger, MontgomeryInt, VanillaInt};
+use ndarray::Array2;
 
 #[test]
 fn test_reducedint_arithmetic() {
@@ -18,10 +20,12 @@ fn test_reducedint_arithmetic() {
     let i2_4 = i0_4.convert(2);
     let i3_4 = i0_4.convert(3);
     assert_eq!(i3_4, i1_4 / i3_4);
-    let result = panic::catch_unwind(|| {
+    assert!(panic::catch_unwind(|| {
         println!("{:?}", i1_4 / i2_4);
-    });
-    assert!(result.is_err());
+    }).is_err());
+    assert!(panic::catch_unwind(|| {
+        i2_4.inv();
+    }).is_err());
 
     // Z/5Z is a field, and 5 is odd, so we can use MontgomeryInt.
     let i0_5 = MontgomeryInt::<u64>::new(0, &5);
@@ -35,4 +39,44 @@ fn test_reducedint_arithmetic() {
     assert_eq!(i4_5 / i3_5, i3_5);
     assert_eq!(i2_5 / i3_5, i4_5);
     assert_eq!(i2_5 / i2_5, i1_5);
+}
+
+#[test]
+fn test_reducedint_array() {
+    let i0_5 = MontgomeryInt::<u64>::new(0, &5);
+    let i1_5 = i0_5.convert(1);
+    let i2_5 = i0_5.convert(2);
+    let i3_5 = i0_5.convert(3);
+    let i4_5 = i0_5.convert(4);
+
+    let arr = Array2::<MontgomeryInt<u64>>::from_shape_vec(
+        (2, 2), vec![i0_5, i1_5, i2_5, i3_5]
+    ).unwrap();
+
+    let arr_2 = arr.clone() * 4;
+    let arr_2_ref = Array2::<MontgomeryInt<u64>>::from_shape_vec(
+        (2, 2), vec![i0_5, i4_5, i3_5, i2_5]
+    ).unwrap();
+    assert_eq!(arr_2, arr_2_ref);
+
+    let arr_3 = arr.clone() + arr_2;
+    let arr_3_ref = Array2::<MontgomeryInt<u64>>::from_shape_vec(
+        (2, 2), vec![i0_5, i0_5, i0_5, i0_5]
+    ).unwrap();
+    assert_eq!(arr_3, arr_3_ref);
+
+    let arr_4 = -arr.clone();
+    assert_eq!(arr_4, arr_2_ref);
+
+    let arr_5 = arr.clone() * 12;
+    let arr_5_ref = Array2::<MontgomeryInt<u64>>::from_shape_vec(
+        (2, 2), vec![i0_5, i2_5, i4_5, i1_5]
+    ).unwrap();
+    assert_eq!(arr_5, arr_5_ref);
+
+    let arr_6 = arr.clone() * i2_5.inv().residue();
+    let arr_6_ref = Array2::<MontgomeryInt<u64>>::from_shape_vec(
+        (2, 2), vec![i0_5, i3_5, i1_5, i4_5]
+    ).unwrap();
+    assert_eq!(arr_6, arr_6_ref);
 }
