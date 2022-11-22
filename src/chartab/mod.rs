19694b14313fe0b8c7c1e51fb91a7c2513ea9120
Bang C. Huynh <bang.cong.huynh@gmail.com>
2022-11-22 00:34:11 +0000

    Implementing CharacterTable.

--- src/chartab/mod.rs
+++ src/chartab/mod.rs
@@ -1,5 +1,6 @@
 use derive_builder::Builder;
 use indexmap::IndexMap;
+use std::fmt::Display;
 
 use crate::chartab::character::Character;
 use crate::symmetry::symmetry_element::symmetry_operation::SymmetryOperation;
@@ -9,9 +10,6 @@ mod character;
 mod modular_linalg;
 mod reducedint;
 mod unityroot;
-
-type RowMap<C> = IndexMap<C, Character>;
-type ColMap<R> = IndexMap<R, Character>;
 
 /// A struct to manage character tables.
 #[derive(Builder)]
@@ -25,31 +23,54 @@ where
 
     /// The characters of the irreducible representations in this group.
     #[builder(setter(custom))]
-    chartab: IndexMap<R, RowMap<C>>,
+    chartab: IndexMap<(R, C), Character>,
 }
 
 impl<R, C> CharacterTable<R, C>
 where
-    R: MathematicalSymbol,
-    C: MathematicalSymbol,
+    R: MathematicalSymbol + Display,
+    C: MathematicalSymbol + Display,
 {
     fn get_character(&self, row: &R, col: &C) -> &Character {
-        self.chartab.get(row).unwrap().get(col).unwrap()
-    }
-
-    fn get_row(&self, row: &R) -> RowMap<C> {
-        self.chartab.get(row).unwrap().clone()
-    }
-
-    fn get_col(&self, col: &C) -> ColMap<R> {
         self.chartab
-            .keys()
-            .map(|row| {
-                (
-                    row.clone(),
-                    self.chartab.get(row).unwrap().get(col).unwrap().clone(),
-                )
+            .iter()
+            .find_map(|((r, c), character)| {
+                if r == row && c == col {
+                    Some(character)
+                } else {
+                    None
+                }
             })
-            .collect::<ColMap<R>>()
+            .expect(format!("No character can be found at ({}, {}).", row, col).as_str())
+    }
+
+    fn get_row(&self, row: &R) -> IndexMap<&C, &Character> {
+        self.chartab
+            .iter()
+            .filter_map(
+                |((r, c), character)| {
+                    if r == row {
+                        Some((c, character))
+                    } else {
+                        None
+                    }
+                },
+            )
+            .collect()
+    }
+
+    fn get_col(&self, col: &C) -> IndexMap<&R, &Character> {
+        self.chartab
+            .iter()
+            .filter_map(
+                |((r, c), character)| {
+                    if c == col {
+                        Some((r, character))
+                    } else {
+                        None
+                    }
+                },
+            )
+            .collect()
     }
 }
