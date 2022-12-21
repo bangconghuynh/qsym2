bb2cffedf45f27060597dc1f089acd56bf9e4bf8
Bang C. Huynh <bang.cong.huynh@gmail.com>
2022-12-21 12:40:46 +0000

    Done with shrotation_3d.

--- src/angmom/shrotation_3d.rs
+++ src/angmom/shrotation_3d.rs
@@ -1,3 +1,4 @@
+use approx;
 use nalgebra::{Rotation3, Unit, Vector3};
 use ndarray::{Array2, Axis, ShapeBuilder};
 
@@ -39,7 +40,7 @@ fn kdelta<T: PartialEq>(i: T, j: T) -> u8 {
 /// # Returns
 ///
 /// The value of $`_iP^l_{\mu m'}`$.
-fn func_P(i: i8, l: u32, mu: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) -> f64 {
+fn func_p(i: i8, l: u32, mu: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) -> f64 {
     assert!(i.abs() <= 1, "i must be between -1 and 1 (inclusive).");
     assert!(l >= 2, "l must be at least 2.");
     let li64 = l as i64;
@@ -72,7 +73,7 @@ fn func_P(i: i8, l: u32, mu: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<
         //  - R[i + 1, -1 + 1] * Rlm1[mu + (l - 1), -l + 1 + (l - 1)]
         rmat[((i + 1) as usize, 2)] * rlm1[((mu + (li64 - 1)) as usize, (l - 1 + (l - 1)) as usize)]
             - rmat[((i + 1) as usize, 0)] * rlm1[((mu + (li64 - 1)) as usize, 0)]
-    } else if mdash == -(li64) {
+    } else if mdash == -li64 {
         // Easier-to-read expression:
         // R[i + 1, 1 + 1] * Rlm1[mu + (l - 1), -l + 1 + (l - 1)]
         //  + R[i + 1, -1 + 1] * Rlm1[mu + (l - 1), l - 1 + (l - 1)]
@@ -106,8 +107,8 @@ fn func_P(i: i8, l: u32, mu: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<
 /// # Returns
 ///
 /// The value of $`U^l_{mm'}`$.
-fn func_U(l: u32, m: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) -> f64 {
-    func_P(0, l, m, mdash, rmat, rlm1)
+fn func_u(l: u32, m: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) -> f64 {
+    func_p(0, l, m, mdash, rmat, rlm1)
 }
 
 /// Returns the function $`V^l_{mm'}`$ as defined in Table 2 of Ivanic, J. & Ruedenberg, K.
@@ -129,7 +130,7 @@ fn func_U(l: u32, m: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) ->
 /// # Returns
 ///
 /// The value of $`V^l_{mm'}`$.
-fn func_V(l: u32, m: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) -> f64 {
+fn func_v(l: u32, m: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) -> f64 {
     assert!(l >= 2, "l must be at least 2.");
     let li64 = l as i64;
     assert!(
@@ -156,13 +157,13 @@ fn func_V(l: u32, m: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) ->
     );
 
     if m > 0 {
-        func_P(1, l, m - 1, mdash, rmat, rlm1) * ((1 + kdelta(m, 1)) as f64).sqrt()
-            - func_P(-1, l, -m + 1, mdash, rmat, rlm1) * ((1 - kdelta(m, 1)) as f64)
+        func_p(1, l, m - 1, mdash, rmat, rlm1) * ((1 + kdelta(m, 1)) as f64).sqrt()
+            - func_p(-1, l, -m + 1, mdash, rmat, rlm1) * ((1 - kdelta(m, 1)) as f64)
     } else if m < 0 {
-        func_P(1, l, m + 1, mdash, rmat, rlm1) * ((1 - kdelta(m, -1)) as f64).sqrt()
-            + func_P(-1, l, -m - 1, mdash, rmat, rlm1) * ((1 + kdelta(m, -1)) as f64)
+        func_p(1, l, m + 1, mdash, rmat, rlm1) * ((1 - kdelta(m, -1)) as f64)
+            + func_p(-1, l, -m - 1, mdash, rmat, rlm1) * ((1 + kdelta(m, -1)) as f64).sqrt()
     } else {
-        func_P(1, l, 1, mdash, rmat, rlm1) + func_P(-1, l, -1, mdash, rmat, rlm1)
+        func_p(1, l, 1, mdash, rmat, rlm1) + func_p(-1, l, -1, mdash, rmat, rlm1)
     }
 }
 
@@ -185,7 +186,7 @@ fn func_V(l: u32, m: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) ->
 /// # Returns
 ///
 /// The value of $`W^l_{mm'}`$.
-fn func_W(l: u32, m: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) -> f64 {
+fn func_w(l: u32, m: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) -> f64 {
     assert!(l >= 2, "l must be at least 2.");
     let li64 = l as i64;
     assert!(
@@ -212,9 +213,9 @@ fn func_W(l: u32, m: i64, mdash: i64, rmat: &Array2<f64>, rlm1: &Array2<f64>) ->
         2 * l as usize - 1
     );
     if m > 0 {
-        func_P(1, l, m + 1, mdash, rmat, rlm1) + func_P(-1, l, -m - 1, mdash, rmat, rlm1)
+        func_p(1, l, m + 1, mdash, rmat, rlm1) + func_p(-1, l, -m - 1, mdash, rmat, rlm1)
     } else {
-        func_P(1, l, m - 1, mdash, rmat, rlm1) - func_P(-1, l, -m + 1, mdash, rmat, rlm1)
+        func_p(1, l, m - 1, mdash, rmat, rlm1) - func_p(-1, l, -m + 1, mdash, rmat, rlm1)
     }
 }
 
@@ -295,7 +296,7 @@ fn coeff_v(l: u32, m: i64, mdash: i64) -> f64 {
     } else {
         (2 * li64) * (2 * li64 - 1)
     };
-    0.5 * ((num as f64) / (den as f64)).sqrt() * ((1 - 2 * kdelta(m, 0)) as f64)
+    0.5 * ((num as f64) / (den as f64)).sqrt() * ((1 - 2 * kdelta(m, 0) as i16) as f64)
 }
 
 /// Returns the coefficient $`w^l_{mm'}`$ as defined in Table 1 of Ivanic, J. & Ruedenberg, K.
@@ -376,4 +377,58 @@ pub fn rmat(angle: f64, axis: Vector3<f64>) -> Array2<f64> {
     rot_array
         .select(Axis(0), &[1, 2, 0])
         .select(Axis(1), &[1, 2, 0])
+}
+
+/// Computes the representation matrix $`\mathbf{R}^l`$ for a transformation of interest in the
+/// basis of real spherical harmonics $`Y_{lm}`$ ordered by increasing $`m`$, as defined in
+/// Equation 5.8 and given in Equation 8.1 of Ivanic, J. & Ruedenberg, K. Rotation Matrices for
+/// Real Spherical Harmonics. Direct Determination by Recursion. *The Journal of Physical
+/// Chemistry* **100**, 9099–9100 (1996), [DOI](https://doi.org/10.1021/jp953350u).
+///
+/// # Arguments
+///
+/// * l - The spherical harmonic order $`l \ge 2`$.
+/// * rmat - The representation matrix of the transformation of interest in the basis of coordinate
+/// *functions* $`(y, z, x)`$, which are isosymmetric to the real spherical harmonics
+/// $`(Y_{1, -1}, Y_{1, 0}, Y_{1, 1})`$.
+/// * rlm1 - The representation matrix of the transformation of interest in the basis of the real
+/// spherical harmonics $`Y_{l-1, m}`$ ordered by increasing $`m`$.
+///
+/// # Returns
+///
+/// The required representation matrix $`\mathbf{R}^l`$.
+pub fn rlmat(l: u32, rmat: &Array2<f64>, rlm1: &Array2<f64>) -> Array2<f64> {
+    assert!(l >= 2, "l must be at least 2.");
+    let li64 = l as i64;
+    let mut rl = Array2::<f64>::zeros((2 * l as usize + 1, 2 * l as usize + 1));
+    for (mi, m) in (-li64..=li64).enumerate() {
+        for (mdashi, mdash) in (-li64..=li64).enumerate() {
+            let cu = coeff_u(l, m, mdash);
+            let f_u = if approx::relative_ne!(cu.abs(), 0.0, epsilon = 1e-14, max_relative = 1e-14)
+            {
+                func_u(l, m, mdash, rmat, rlm1)
+            } else {
+                0.0
+            };
+
+            let cv = coeff_v(l, m, mdash);
+            let f_v = if approx::relative_ne!(cv.abs(), 0.0, epsilon = 1e-14, max_relative = 1e-14)
+            {
+                func_v(l, m, mdash, rmat, rlm1)
+            } else {
+                0.0
+            };
+
+            let cw = coeff_w(l, m, mdash);
+            let f_w = if approx::relative_ne!(cw.abs(), 0.0, epsilon = 1e-14, max_relative = 1e-14)
+            {
+                func_w(l, m, mdash, rmat, rlm1)
+            } else {
+                0.0
+            };
+
+            rl[(mi, mdashi)] = cu * f_u + cv * f_v + cw * f_w;
+        }
+    }
+    rl
 }
--- src/angmom/shrotation_3d_tests.rs
+++ src/angmom/shrotation_3d_tests.rs
@@ -1,23 +1,99 @@
 use approx;
-use ndarray::array;
 use nalgebra::Vector3;
+use ndarray::array;
 
-use crate::angmom::shrotation_3d::rmat;
+use crate::angmom::shrotation_3d::{rlmat, rmat};
 
 #[test]
 fn test_shrotation_3d_rmat() {
-    let rmat_pi_6_z = rmat(std::f64::consts::FRAC_PI_6, Vector3::new(0.0, 0.0, 1.0));
     let sq3 = 3.0f64.sqrt();
+
+    let rmat_pi_6_z = rmat(std::f64::consts::FRAC_PI_6, Vector3::new(0.0, 0.0, 1.0));
     let rmat_pi_6_z_ref = array![
-        [sq3 / 2.0, 0.0,       0.5],
-        [      0.0, 1.0,       0.0],
-        [     -0.5, 0.0, sq3 / 2.0],
+        [sq3 / 2.0, 0.0, 0.5],
+        [0.0, 1.0, 0.0],
+        [-0.5, 0.0, sq3 / 2.0],
     ];
     approx::assert_relative_eq!(
-        (rmat_pi_6_z - rmat_pi_6_z_ref)
-            .map(|x| x * x)
-            .sum()
-            .sqrt(),
+        (rmat_pi_6_z - rmat_pi_6_z_ref).map(|x| x * x).sum().sqrt(),
+        0.0,
+        epsilon = 1e-14,
+        max_relative = 1e-14
+    );
+
+    let rmat_pi_3_y = rmat(std::f64::consts::FRAC_PI_3, Vector3::new(0.0, 1.0, 0.0));
+    let rmat_pi_3_y_ref = array![
+        [1.0, 0.0, 0.0],
+        [0.0, 0.5, -sq3 / 2.0],
+        [0.0, sq3 / 2.0, 0.5],
+    ];
+    approx::assert_relative_eq!(
+        (rmat_pi_3_y - rmat_pi_3_y_ref).map(|x| x * x).sum().sqrt(),
+        0.0,
+        epsilon = 1e-14,
+        max_relative = 1e-14
+    );
+
+    let rmat_pi_2_x = rmat(std::f64::consts::FRAC_PI_2, Vector3::new(1.0, 0.0, 0.0));
+    let rmat_pi_2_x_ref = array![[0.0, -1.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, 1.0],];
+    approx::assert_relative_eq!(
+        (rmat_pi_2_x - rmat_pi_2_x_ref).map(|x| x * x).sum().sqrt(),
+        0.0,
+        epsilon = 1e-14,
+        max_relative = 1e-14
+    );
+}
+
+#[test]
+fn test_shrotation_3d_rlmat() {
+    let r = rmat(std::f64::consts::FRAC_PI_2, Vector3::new(0.0, 0.0, 1.0));
+    let r2 = rlmat(2, &r, &r);
+    let r2_ref = array![
+        [-1.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0, 0.0],
+        [0.0, -1.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, -1.0]
+    ];
+    approx::assert_relative_eq!(
+        (&r2 - &r2_ref).map(|x| x * x).sum().sqrt(),
+        0.0,
+        epsilon = 1e-14,
+        max_relative = 1e-14
+    );
+
+    let r3 = rlmat(3, &r, &r2);
+    let r3_ref = array![
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+        [0.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0],
+        [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    ];
+    approx::assert_relative_eq!(
+        (&r3 - &r3_ref).map(|x| x * x).sum().sqrt(),
+        0.0,
+        epsilon = 1e-14,
+        max_relative = 1e-14
+    );
+
+    let r = rmat(
+        2.0 * std::f64::consts::FRAC_PI_3,
+        Vector3::new(1.0, 1.0, 1.0),
+    );
+    let r2 = rlmat(2, &r, &r);
+    let sq3 = 3.0f64.sqrt();
+    let r2_ref = array![
+        [0.0, 0.0, 0.0, 1.0, 0.0],
+        [1.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, -0.5, 0.0, -sq3 / 2.0],
+        [0.0, 1.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, sq3 / 2.0, 0.0, -0.5]
+    ];
+    approx::assert_relative_eq!(
+        (&r2 - &r2_ref).map(|x| x * x).sum().sqrt(),
         0.0,
         epsilon = 1e-14,
         max_relative = 1e-14
