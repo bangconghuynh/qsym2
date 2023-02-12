e39ba77af23d4bff54eb655ed818e2f8329c7cc3
Bang C. Huynh <pczbh1@archon.nottingham.ac.uk>
2023-02-12 02:31:30 +0000

    Debugging...

--- src/permutation/permutation_group_tests.rs
+++ src/permutation/permutation_group_tests.rs
@@ -1,12 +1,11 @@
 use env_logger;
 
-use crate::group::UnitaryRepresentedGroup;
 use crate::chartab::chartab_group::CharacterProperties;
 use crate::permutation::permutation_group::{PermutationGroupProperties, PermutationGroup};
 
 #[test]
 fn test_permutation_group_construction() {
     env_logger::init();
-    let sym3 = PermutationGroup::from_rank(8);
+    let sym3 = PermutationGroup::from_rank(10);
     println!("{:?}", sym3.character_table());
 }
