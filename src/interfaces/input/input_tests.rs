4545c7fdc96ae24a4f4eb70f8c55ac6c55ed1b38
Bang C. Huynh <pczbh1@archon.nottingham.ac.uk>
2023-06-26 01:38:47 +0100

    Symmetry group detection can now accept a file.

--- src/interfaces/input/input_tests.rs
+++ src/interfaces/input/input_tests.rs
@@ -1,70 +1,92 @@
 use nalgebra::{Point3, Vector3};
 
 use crate::drivers::representation_analysis::CharacterTableDisplay;
-use crate::io::read_qsym2_yaml;
+use crate::io::{read_qsym2_yaml, write_qsym2_yaml};
 use crate::symmetry::symmetry_transformation::SymmetryTransformationKind;
 
-use super::Input;
+use super::{Input, SymmetryGroupDetectionInputKind};
 
 const ROOT: &str = env!("CARGO_MANIFEST_DIR");
 
 #[test]
-fn test_interfaces_input_serde_yaml() {
-    let name = format!("{ROOT}/tests/input/test_input.yml");
+fn test_interfaces_input_symmetry_group_detection_parameters() {
+    let name = format!("{ROOT}/tests/input/test_input_symmetry_group_detection_parameters.yml");
     let inp = read_qsym2_yaml::<Input>(&name).unwrap();
 
-    assert_eq!(
-        inp.symmetry_group_detection.moi_thresholds,
-        vec![1e-4, 1e-5, 1e-6]
-    );
-    assert_eq!(
-        inp.symmetry_group_detection.distance_thresholds,
-        vec![1e-5, 1e-6]
-    );
-    assert!(inp.symmetry_group_detection.time_reversal);
-    assert_eq!(
-        inp.symmetry_group_detection
-            .fictitious_magnetic_fields
-            .as_ref()
-            .unwrap()[0]
-            .0,
-        Point3::new(0.0, 1.0, 0.0)
-    );
-    assert_eq!(
-        inp.symmetry_group_detection
-            .fictitious_magnetic_fields
-            .as_ref()
-            .unwrap()[0]
-            .1,
-        Vector3::new(1.0, 0.0, 0.0)
-    );
-    assert!(inp
-        .symmetry_group_detection
-        .fictitious_electric_fields
-        .is_none());
-    assert!(inp.symmetry_group_detection.field_origin_com);
-    assert!(!inp.symmetry_group_detection.write_symmetry_elements);
-    assert!(inp.symmetry_group_detection.result_save_name.is_none());
+    if let SymmetryGroupDetectionInputKind::Parameters(inp_pd_params) =
+        inp.symmetry_group_detection.unwrap()
+    {
+        assert_eq!(inp_pd_params.moi_thresholds, vec![1e-4, 1e-5, 1e-6]);
+        assert_eq!(inp_pd_params.distance_thresholds, vec![1e-5, 1e-6]);
+        assert!(inp_pd_params.time_reversal);
+        assert_eq!(
+            inp_pd_params.fictitious_magnetic_fields.as_ref().unwrap()[0].0,
+            Point3::new(0.0, 1.0, 0.0)
+        );
+        assert_eq!(
+            inp_pd_params.fictitious_magnetic_fields.as_ref().unwrap()[0].1,
+            Vector3::new(1.0, 0.0, 0.0)
+        );
+        assert!(inp_pd_params.fictitious_electric_fields.is_none());
+        assert!(inp_pd_params.field_origin_com);
+        assert!(!inp_pd_params.write_symmetry_elements);
+        assert!(inp_pd_params.result_save_name.is_none());
+    } else {
+        assert!(false);
+    }
 
-    assert_eq!(inp.representation_analysis.integrality_threshold, 1e-8);
+    let inp_rep_params = inp.det_representation_analysis.unwrap();
+    assert_eq!(inp_rep_params.integrality_threshold, 1e-8);
     assert_eq!(
-        inp.representation_analysis.linear_independence_threshold,
+        inp_rep_params.linear_independence_threshold,
         1e-7
     );
-    assert!(inp.representation_analysis.analyse_mo_symmetries);
-    assert!(!inp.representation_analysis.use_magnetic_group);
-    assert!(!inp.representation_analysis.use_double_group);
+    assert!(inp_rep_params.analyse_mo_symmetries);
+    assert!(!inp_rep_params.use_magnetic_group);
+    assert!(!inp_rep_params.use_double_group);
     assert!(matches!(
-        inp.representation_analysis.symmetry_transformation_kind,
+        inp_rep_params.symmetry_transformation_kind,
         SymmetryTransformationKind::Spatial
     ));
     assert!(matches!(
-        inp.representation_analysis.write_character_table,
+        inp_rep_params.write_character_table,
         Some(CharacterTableDisplay::Numerical)
     ));
-    assert!(inp.representation_analysis.write_overlap_eigenvalues);
+    assert!(inp_rep_params.write_overlap_eigenvalues);
     assert_eq!(
-        inp.representation_analysis.infinite_order_to_finite,
+        inp_rep_params.infinite_order_to_finite,
         Some(8)
     );
+}
+
+#[test]
+fn test_interfaces_input_symmetry_group_detection_fromfile() {
+    let name = format!("{ROOT}/tests/input/test_input_symmetry_group_detection_fromfile.yml");
+    let inp = read_qsym2_yaml::<Input>(&name).unwrap();
+
+    if let SymmetryGroupDetectionInputKind::FromFile(name) = inp.symmetry_group_detection.unwrap() {
+        assert_eq!(name, "test_file")
+    } else {
+        assert!(false);
+    }
+
+    let inp_rep_params = inp.det_representation_analysis.unwrap();
+    assert_eq!(inp_rep_params.integrality_threshold, 1e-7);
+    assert_eq!(
+        inp_rep_params.linear_independence_threshold,
+        1e-7
+    );
+    assert!(inp_rep_params.analyse_mo_symmetries);
+    assert!(!inp_rep_params.use_magnetic_group);
+    assert!(!inp_rep_params.use_double_group);
+    assert!(matches!(
+        inp_rep_params.symmetry_transformation_kind,
+        SymmetryTransformationKind::Spatial
+    ));
+    assert!(matches!(
+        inp_rep_params.write_character_table,
+        Some(CharacterTableDisplay::Symbolic)
+    ));
+    assert!(inp_rep_params.write_overlap_eigenvalues);
+    assert!(inp_rep_params.infinite_order_to_finite.is_none());
 }
--- src/interfaces/input/mod.rs
+++ src/interfaces/input/mod.rs
@@ -2,14 +2,60 @@ use serde::{Deserialize, Serialize};
 
 use crate::drivers::representation_analysis::slater_determinant::SlaterDeterminantRepAnalysisParams;
 use crate::drivers::symmetry_group_detection::SymmetryGroupDetectionParams;
+#[allow(unused_imports)]
+use crate::io::QSym2FileType;
 
-#[derive(Clone, Serialize, Deserialize, Default)]
+/// An enumerated type representing possible input kinds for symmetry-group detection from a YAML
+/// input file.
+#[derive(Clone, Serialize, Deserialize)]
+enum SymmetryGroupDetectionInputKind {
+    /// Variant indicating that the parameters for the symmetry-group detection driver will be
+    /// specified.
+    Parameters(SymmetryGroupDetectionParams),
+
+    /// Variant indicating that the symmetry-group detection results will be read in from a `QSym2`
+    /// [`QSym2FileType:Sym`] binary file. The associated string gives the name of the file without
+    /// its `.qsym2.sym` extension.
+    FromFile(String),
+}
+
+impl Default for SymmetryGroupDetectionInputKind {
+    fn default() -> Self {
+        SymmetryGroupDetectionInputKind::Parameters(SymmetryGroupDetectionParams::default())
+    }
+}
+
+/// A structure containing `QSym2` input parameters which can be serialised into and deserialised
+/// from a YAML input file.
+#[derive(Clone, Serialize, Deserialize)]
 struct Input {
+    /// Specification for symmetry-group detection. If `None`, no symmetry-group detection will be
+    /// performed. If not `None`, then this either specifies the parameters for symmetry-group
+    /// detection, or the name of a [`QSym2FileType:Sym`] binary file containing the symmetry-group
+    /// detection results (without the `.qsym2.sym` extension).
+    ///
+    /// If not specified, this will be taken to be `None`.
     #[serde(default)]
-    symmetry_group_detection: SymmetryGroupDetectionParams,
+    symmetry_group_detection: Option<SymmetryGroupDetectionInputKind>,
 
+    /// Specification for Slater determinant representation analysis. If `None`, no Slater
+    /// determinant representation analysis will be performed. If not `None`, then this specifies
+    /// the parameters for Slater determinant representation analysis.
+    ///
+    /// # Default
+    ///
+    /// If not specified, this will be taken to be `None`.
     #[serde(default)]
-    representation_analysis: SlaterDeterminantRepAnalysisParams<f64>,
+    det_representation_analysis: Option<SlaterDeterminantRepAnalysisParams<f64>>,
+}
+
+impl Default for Input {
+    fn default() -> Self {
+        Input {
+            symmetry_group_detection: Some(SymmetryGroupDetectionInputKind::default()),
+            det_representation_analysis: Some(SlaterDeterminantRepAnalysisParams::<f64>::default()),
+        }
+    }
 }
 
 #[cfg(test)]
