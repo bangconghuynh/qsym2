8b8a6ba403c7b46dd86abf5dc2b6f5b847b159f4
Bang C. Huynh <pczbh1@archon.nottingham.ac.uk>
2023-09-11 17:54:34 +0100

    Added tests for vibrational coordinate rep analysis driver using mag-rep groups.

--- src/drivers/representation_analysis/vibrational_coordinate_tests.rs
+++ src/drivers/representation_analysis/vibrational_coordinate_tests.rs
@@ -3,7 +3,6 @@ use nalgebra::{Point3, Vector3};
 use ndarray::array;
 use num_complex::Complex;
 
-use crate::auxiliary::molecule::Molecule;
 use crate::chartab::chartab_symbols::DecomposedSymbol;
 use crate::drivers::representation_analysis::angular_function::AngularFunctionRepAnalysisParams;
 use crate::drivers::representation_analysis::vibrational_coordinate::{
@@ -78,9 +77,9 @@ fn test_drivers_vibrational_coordinate_analysis_xef4() {
         .build()
         .unwrap();
 
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // u Oh (ordinary, unitary) - spin spatial
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // u Oh (ordinary, unitary) - spatial
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     let vca_params = VibrationalCoordinateRepAnalysisParams::<f64>::builder()
         .integrality_threshold(1e-3)
         .linear_independence_threshold(1e-3)
@@ -237,9 +236,9 @@ fn test_drivers_vibrational_coordinate_analysis_xef4_magnetic_field() {
         .build()
         .unwrap();
 
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // u Oh (ordinary, unitary) - spin spatial
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // u C4h (ordinary, unitary) - spatial
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     let vca_params = VibrationalCoordinateRepAnalysisParams::<f64>::builder()
         .integrality_threshold(1e-3)
         .linear_independence_threshold(1e-3)
@@ -296,7 +295,7 @@ fn test_drivers_vibrational_coordinate_analysis_xef4_magnetic_field() {
         DecomposedSymbol::<MullikenIrrepSymbol>::new("|_(a)|Γ|_(u)| ⊕ |_(b)|Γ|_(u)|").unwrap()
     );
 
-    let vibs_c: VibrationalCoordinateCollection<C128> = vibs.into();
+    let vibs_c: VibrationalCoordinateCollection<C128> = vibs.clone().into();
     let mut vca_driver_c =
         VibrationalCoordinateRepAnalysisDriver::<UnitaryRepresentedSymmetryGroup, C128>::builder()
             .parameters(&vca_params)
@@ -341,5 +340,109 @@ fn test_drivers_vibrational_coordinate_analysis_xef4_magnetic_field() {
             .as_ref()
             .unwrap(),
         DecomposedSymbol::<MullikenIrrepSymbol>::new("|_(a)|Γ|_(u)| ⊕ |_(b)|Γ|_(u)|").unwrap()
+    );
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // m D4h(C4h) (bw, magnetic) - spatial
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    let vca_params = VibrationalCoordinateRepAnalysisParams::<f64>::builder()
+        .integrality_threshold(1e-3)
+        .linear_independence_threshold(1e-3)
+        .use_magnetic_group(Some(MagneticSymmetryAnalysisKind::Corepresentation))
+        .use_double_group(false)
+        .symmetry_transformation_kind(SymmetryTransformationKind::Spatial)
+        .write_character_table(Some(CharacterTableDisplay::Symbolic))
+        .build()
+        .unwrap();
+    let mut vca_driver =
+        VibrationalCoordinateRepAnalysisDriver::<MagneticRepresentedSymmetryGroup, f64>::builder()
+            .parameters(&vca_params)
+            .angular_function_parameters(&afa_params)
+            .vibrational_coordinate_collection(&vibs)
+            .symmetry_group(pd_res)
+            .build()
+            .unwrap();
+    assert!(vca_driver.run().is_ok());
+    assert_eq!(
+        *vca_driver
+            .result()
+            .unwrap()
+            .vibrational_coordinate_symmetries()[0]
+            .as_ref()
+            .unwrap(),
+        DecomposedSymbol::<MullikenIrcorepSymbol>::new("||A|_(u)|").unwrap()
+    );
+    assert_eq!(
+        *vca_driver
+            .result()
+            .unwrap()
+            .vibrational_coordinate_symmetries()[1]
+            .as_ref()
+            .unwrap(),
+        DecomposedSymbol::<MullikenIrcorepSymbol>::new("||B|_(u)|").unwrap()
+    );
+    assert_eq!(
+        *vca_driver
+            .result()
+            .unwrap()
+            .vibrational_coordinate_symmetries()[2]
+            .as_ref()
+            .unwrap(),
+        DecomposedSymbol::<MullikenIrcorepSymbol>::new("|_(a)|Γ|_(u)| ⊕ |_(b)|Γ|_(u)|").unwrap()
+    );
+    assert_eq!(
+        *vca_driver
+            .result()
+            .unwrap()
+            .vibrational_coordinate_symmetries()[3]
+            .as_ref()
+            .unwrap(),
+        DecomposedSymbol::<MullikenIrcorepSymbol>::new("|_(a)|Γ|_(u)| ⊕ |_(b)|Γ|_(u)|").unwrap()
+    );
+
+    let mut vca_driver_c =
+        VibrationalCoordinateRepAnalysisDriver::<MagneticRepresentedSymmetryGroup, C128>::builder()
+            .parameters(&vca_params)
+            .angular_function_parameters(&afa_params)
+            .vibrational_coordinate_collection(&vibs_c)
+            .symmetry_group(pd_res)
+            .build()
+            .unwrap();
+    assert!(vca_driver_c.run().is_ok());
+    assert_eq!(
+        *vca_driver_c
+            .result()
+            .unwrap()
+            .vibrational_coordinate_symmetries()[0]
+            .as_ref()
+            .unwrap(),
+        DecomposedSymbol::<MullikenIrcorepSymbol>::new("||A|_(u)|").unwrap()
+    );
+    assert_eq!(
+        *vca_driver_c
+            .result()
+            .unwrap()
+            .vibrational_coordinate_symmetries()[1]
+            .as_ref()
+            .unwrap(),
+        DecomposedSymbol::<MullikenIrcorepSymbol>::new("||B|_(u)|").unwrap()
+    );
+    assert_eq!(
+        *vca_driver_c
+            .result()
+            .unwrap()
+            .vibrational_coordinate_symmetries()[2]
+            .as_ref()
+            .unwrap(),
+        DecomposedSymbol::<MullikenIrcorepSymbol>::new("|_(a)|Γ|_(u)| ⊕ |_(b)|Γ|_(u)|").unwrap()
+    );
+    assert_eq!(
+        *vca_driver_c
+            .result()
+            .unwrap()
+            .vibrational_coordinate_symmetries()[3]
+            .as_ref()
+            .unwrap(),
+        DecomposedSymbol::<MullikenIrcorepSymbol>::new("|_(a)|Γ|_(u)| ⊕ |_(b)|Γ|_(u)|").unwrap()
     );
 }
