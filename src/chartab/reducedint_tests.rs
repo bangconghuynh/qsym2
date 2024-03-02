4023a662b8acbd24d94965c7b471c4d5c66219ee
Bang C. Huynh <pczbh1@archon.nottingham.ac.uk>
2024-03-02 16:05:15 +0000

    Upgraded all dependencies to latest versions.

--- src/chartab/reducedint_tests.rs
+++ src/chartab/reducedint_tests.rs
@@ -83,12 +83,12 @@ fn test_linalgreducedint_identities() {
     assert_eq!(i4_7.pow(0), one);
 
     // Inverse
-    assert_eq!(one / i5_7, i5_7.inv());
-    assert_eq!(i1_7 / i3_7, i3_7.inv());
-    assert_eq!(i4_7.inv(), i2_7);
-    assert_eq!(one.inv(), one);
-    assert!(panic::catch_unwind(|| { zero.inv() }).is_err());
-    assert!(panic::catch_unwind(|| { i0_7.inv() }).is_err());
+    assert_eq!(one / i5_7, i5_7.inv().unwrap());
+    assert_eq!(i1_7 / i3_7, i3_7.inv().unwrap());
+    assert_eq!(i4_7.inv().unwrap(), i2_7);
+    assert_eq!(one.inv().unwrap(), one);
+    assert!(zero.inv().is_none());
+    assert!(i0_7.inv().is_none());
 }
 
 #[test]
@@ -144,10 +144,7 @@ fn test_linalgreducedint_arithmetic() {
         println!("{:?}", i1_4 / i2_4);
     })
     .is_err());
-    assert!(panic::catch_unwind(|| {
-        i2_4.inv();
-    })
-    .is_err());
+    assert!(i2_4.inv().is_none());
 
     // Z/5Z is a field, and 5 is odd, so we can use MontgomeryInt.
     let i0_5 = MontgomeryInt::<u64>::new(0, &5).linalg();
@@ -205,7 +202,7 @@ fn test_linalgreducedint_array() {
     let arr_5_ref = array![[i0_5, i2_5], [i4_5, i1_5]];
     assert_eq!(arr_5, arr_5_ref);
 
-    let arr_6 = arr * i2_5.inv().residue();
+    let arr_6 = arr * i2_5.inv().unwrap().residue();
     let arr_6_ref = array![[i0_5, i3_5], [i1_5, i4_5]];
     assert_eq!(arr_6, arr_6_ref);
 }
