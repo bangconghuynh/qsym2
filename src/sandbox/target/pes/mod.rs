638c89fd83b58e42b3862cd9326c35e5e6a5d4a5
Bang C. Huynh <pczbh1@archon.nottingham.ac.uk>
2024-04-11 00:25:32 +0100

    Halfway through PES with fn.

--- src/sandbox/target/pes/mod.rs
+++ src/sandbox/target/pes/mod.rs
@@ -1,18 +1,14 @@
 //! Potential energy surfaces.
 
 use std::fmt;
-use std::iter::Sum;
 
 use derive_builder::Builder;
 use itertools::Itertools;
 use log;
 use nalgebra::Point3;
-use ndarray::Array2;
 use ndarray_linalg::types::Lapack;
 use num_complex::{Complex, ComplexFloat};
 use num_traits::float::{Float, FloatConst};
-
-use crate::group::GroupProperties;
 
 #[cfg(test)]
 mod pes_tests;
@@ -24,90 +20,54 @@ mod pes_transformation;
 // Struct definitions
 // ==================
 
-/// Structure to manage potential energy surfaces evaluated at symmetry-equivalent points.
+/// Structure to manage potential energy surfaces.
 #[derive(Builder, Clone)]
 #[builder(build_fn(validate = "Self::validate"))]
-pub struct PES<'a, G, T>
+pub struct PES<T>
 where
     T: ComplexFloat + Lapack,
-    G: GroupProperties + Clone,
 {
-    /// The group governing the symmetry-equivalent points at which this PES has been evaluated.
-    group: &'a G,
-
-    /// The symmetry-unique grid points $`\mathbf{r}_j`$ at which the PES is evaluated.
+    /// The grid points $`\mathbf{r}_j`$ at which the PES is to be evaluated.
     grid_points: Vec<Point3<f64>>,
 
-    /// The values of this PES evaluated at symmetry-equivalent points. The elements `$V_{ij}$` is
-    /// given by $`V(\hat{g}_i \mathbf{r}_j)`$, where $`\mathbf{r}_j`$ is a symmetry-unique grid
-    /// point at which the PES is evaluated.
-    values: Array2<T>,
+    /// The function $`\mathbb{R}^3 \to T`$ defining the PES.
+    function: fn(&Point3<f64>) -> T,
 }
 
-impl<'a, G, T> PESBuilder<'a, G, T>
+impl<T> PESBuilder<T>
 where
     T: ComplexFloat + Lapack,
-    G: GroupProperties + Clone,
 {
     fn validate(&self) -> Result<(), String> {
-        let group = self.group.ok_or("No symmetry group found.".to_string())?;
         let grid_points = self
             .grid_points
             .as_ref()
-            .ok_or("No symmetry-unique grid points found.".to_string())?;
-        let values = self.values.as_ref().ok_or("No PES found.".to_string())?;
-
-        let pes_shape = group.order() == values.shape()[0];
-        if !pes_shape {
-            log::error!(
-                "The number of rows in the PES value matrix ({:?}) does not match the group order ({}).",
-                values.shape()[0],
-                group.order()
-            );
-        }
-
-        let grid_count = grid_points.len() == values.shape()[1];
-        if !grid_count {
-            log::error!(
-                "The number of columns in the PES value matrix ({:?}) does not match the number of grid points ({}).",
-                values.shape()[1],
-                grid_points.len()
-            );
-        }
-
-        if pes_shape && grid_count {
-            Ok(())
-        } else {
-            Err("PES validation failed.".to_string())
-        }
+            .ok_or("No grid points found.".to_string())?;
+        let function = self
+            .function
+            .as_ref()
+            .ok_or("No PES function found.".to_string())?;
+        Ok(())
     }
 }
 
-impl<'a, G, T> PES<'a, G, T>
+impl<T> PES<T>
 where
     T: ComplexFloat + Clone + Lapack,
-    G: GroupProperties + Clone,
 {
     /// Returns a builder to construct a new [`PES`].
-    pub fn builder() -> PESBuilder<'a, G, T> {
+    pub fn builder() -> PESBuilder<T> {
         PESBuilder::default()
     }
 
-    /// Returns a vector of shared references to the symmetry-unique grid points at which the PES
-    /// is evaluated.
+    /// Returns a vector of shared references to the grid points at which the PES is evaluated.
     pub fn grid_points(&self) -> Vec<&Point3<f64>> {
         self.grid_points.iter().collect_vec()
     }
 
-    /// Returns a shared reference to the value matrix of the PES.
-    pub fn values(&self) -> &Array2<T> {
-        &self.values
-    }
-
-    /// Returns a shared reference to the group governing the symmetry-equivalent points at which
-    /// the PES is evaluated.
-    pub fn group(&self) -> &G {
-        self.group
+    /// Returns a shared reference to the function defining the PES.
+    pub fn function(&self) -> &fn(&Point3<f64>) -> T {
+        &self.function
     }
 }
 
@@ -118,17 +78,15 @@ where
 // ----
 // From
 // ----
-impl<'a, G, T> From<PES<'a, G, T>> for PES<'a, G, Complex<T>>
+impl<T> From<PES<T>> for PES<Complex<T>>
 where
     T: Float + FloatConst + Lapack,
     Complex<T>: Lapack,
-    G: GroupProperties + Clone,
 {
-    fn from(value: PES<'a, G, T>) -> Self {
-        PES::<'a, G, Complex<T>>::builder()
+    fn from(value: PES<T>) -> Self {
+        PES::<Complex<T>>::builder()
             .grid_points(value.grid_points.clone())
-            .values(value.values.map(Complex::from))
-            .group(value.group)
+            .function(|pt| Complex::from(value.function()(pt)))
             .build()
             .expect("Unable to complexify a `PES`.")
     }
@@ -137,22 +95,20 @@ where
 // -------
 // Display
 // -------
-impl<'a, G, T> fmt::Display for PES<'a, G, T>
+impl<T> fmt::Display for PES<T>
 where
-    T: fmt::Display + ComplexFloat + Lapack,
-    <T as ComplexFloat>::Real: Sum + From<u16> + fmt::Display,
-    G: GroupProperties + Clone,
+    T: ComplexFloat + Lapack,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "PES[value matrix of dimensions {}]",
-            self.values
-                .shape()
-                .iter()
-                .map(|x| x.to_string())
-                .collect::<Vec<_>>()
-                .join("×")
+            "PES[grid of {} {}]",
+            self.grid_points.len(),
+            if self.grid_points.len() == 1 {
+                "point"
+            } else {
+                "points"
+            }
         )?;
         Ok(())
     }
--- src/sandbox/target/pes/pes_transformation.rs
+++ src/sandbox/target/pes/pes_transformation.rs
@@ -1,17 +1,15 @@
 //! Implementation of symmetry transformations for PESes.
 
-use anyhow::format_err;
 use itertools::Itertools;
-use ndarray::{s, Array2, Axis};
-use ndarray_linalg::norm::Norm;
+use nalgebra::Point3;
+use ndarray::{Array2, Axis, ShapeBuilder};
 use ndarray_linalg::types::Lapack;
+use ndarray_linalg::Inverse;
 use num_complex::{Complex, ComplexFloat};
 
 use crate::permutation::Permutation;
 use crate::sandbox::target::pes::PES;
 use crate::symmetry::symmetry_element::SymmetryOperation;
-use crate::symmetry::symmetry_element::symmetry_operation::SpecialSymmetryTransformation;
-use crate::symmetry::symmetry_group::SymmetryGroupProperties;
 use crate::symmetry::symmetry_transformation::{
     ComplexConjugationTransformable, DefaultTimeReversalTransformable, SpatialUnitaryTransformable,
     SpinUnitaryTransformable, SymmetryTransformable, TimeReversalTransformable,
@@ -21,37 +19,26 @@ use crate::symmetry::symmetry_transformation::{
 // ---------------------------
 // SpatialUnitaryTransformable
 // ---------------------------
-impl<'a, G, T> SpatialUnitaryTransformable for PES<'a, G, T>
+impl<'a, T> SpatialUnitaryTransformable for PES<'a, T>
 where
     T: ComplexFloat + Lapack,
-    G: SymmetryGroupProperties + Clone,
 {
-    /// Note that this is potentially slow since `rmat` instead of the actual group element is given.
     fn transform_spatial_mut(
         &mut self,
         rmat: &Array2<f64>,
         _: Option<&Permutation<usize>>,
     ) -> Result<&mut Self, anyhow::Error> {
-        let gi = self.group().elements().clone().into_iter().position(|g| {
-            (rmat - g.get_3d_spatial_matrix()).norm_l2() < 1e-7
-        }).ok_or(format_err!("Unable to find the symmetry operation in the PES specification that matches the required transformation matrix."))?;
-        let transformed_values = self
-            .group()
-            .cayley_table()
-            .ok_or(format_err!("Cayley table not found in the symmetry group associated with the PES specification."))
-            .and_then(|ctb| {
-                ctb
-                    .slice(s![.., gi])
-                    .iter()
-                    .position(|&x| x == 0)
-                    .ok_or(format_err!(
-                        "Unable to find the inverse of group element `{gi}`."
-                    )).map(|giinv| {
-                        let giinv_perm = ctb.slice(s![giinv, ..]).iter().cloned().collect_vec();
-                        self.values().select(Axis(0), &giinv_perm)
-                    })
-            })?;
-        self.values = transformed_values;
+        let rmat = rmat.select(Axis(0), &[2, 0, 1]).select(Axis(1), &[2, 0, 1]);
+        let rmatinv = rmat.inv()?;
+        let grid_array: Array2<f64> =
+            Array2::<f64>::from_iter(self.grid_points.iter().flat_map(|pt| pt.iter()))
+                .reshape((3, self.grid_points.len()).f());
+        let rmatinv_grid_array = rmatinv.dot(&grid_array);
+        let rmatinv_grid_points = rmatinv_grid_array
+            .columns()
+            .map(|col| Point3::new(col[0], col[1], col[2]))
+            .collect_vec();
+        self.grid_points = rmatinv_grid_points;
         Ok(self)
     }
 }
@@ -60,10 +47,9 @@ where
 // SpinUnitaryTransformable
 // ------------------------
 
-impl<'a, G, T> SpinUnitaryTransformable for PES<'a, G, T>
+impl<'a, T> SpinUnitaryTransformable for PES<'a, T>
 where
     T: ComplexFloat + Lapack,
-    G: SymmetryGroupProperties + Clone,
 {
     /// Performs a spin transformation in-place.
     ///
@@ -81,13 +67,12 @@ where
 // ComplexConjugationTransformable
 // -------------------------------
 
-impl<'a, G, T> ComplexConjugationTransformable for PES<'a, G, T>
+impl<'a, T> ComplexConjugationTransformable for PES<'a, T>
 where
     T: ComplexFloat + Lapack,
-    G: SymmetryGroupProperties + Clone,
 {
     fn transform_cc_mut(&mut self) -> &mut Self {
-        self.values.mapv_inplace(|x| x.conj());
+        self.function = |pt: &Point3<f64>| self.function()(pt).conj();
         self
     }
 }
@@ -95,21 +80,19 @@ where
 // --------------------------------
 // DefaultTimeReversalTransformable
 // --------------------------------
-impl<'a, G, T> DefaultTimeReversalTransformable for PES<'a, G, T>
+impl<'a, T> DefaultTimeReversalTransformable for PES<'a, T>
 where
     T: ComplexFloat + Lapack,
-    G: SymmetryGroupProperties + Clone,
 {
 }
 
 // ---------------------
 // SymmetryTransformable
 // ---------------------
-impl<'a, G, T> SymmetryTransformable for PES<'a, G, T>
+impl<'a, T> SymmetryTransformable for PES<'a, T>
 where
     T: ComplexFloat + Lapack,
-    G: SymmetryGroupProperties + Clone,
-    PES<'a, G, T>: SpatialUnitaryTransformable + TimeReversalTransformable,
+    PES<'a, T>: SpatialUnitaryTransformable + TimeReversalTransformable,
 {
     /// PESes have no local sites for permutation. This method therefore simply returns the
     /// identity permutation on one object.
@@ -118,69 +101,5 @@ where
         _: &SymmetryOperation,
     ) -> Result<Permutation<usize>, TransformationError> {
         Permutation::from_image(vec![0]).map_err(|err| TransformationError(err.to_string()))
-    }
-
-    /// Performs a spatial transformation according to a specified symmetry operation in-place.
-    ///
-    /// Note that both $`\mathsf{SO}(3)`$ and $`\mathsf{SU}(2)`$ rotations effect the same spatial
-    /// transformation. Also note that, if the transformation contains time reversal, it will be
-    /// accompanied by a complex conjugation.
-    ///
-    /// This is a specialised implementation for [`PES`] since the default implementation of this
-    /// method for the [`SymmetryTransformable`] trait makes use of [`Self::transform_spatial_mut`]
-    /// which is inefficient for [`PES`].
-    ///
-    /// # Arguments
-    ///
-    /// * `symop` - A symmetry operation.
-    fn sym_transform_spatial_mut(
-        &mut self,
-        symop: &SymmetryOperation,
-    ) -> Result<&mut Self, TransformationError> {
-        let gi = self
-            .group()
-            .get_index_of(symop)
-            .ok_or(TransformationError("Unable to find the symmetry operation in the PES specification that matches the required transformation matrix.".to_string()))?;
-        let transformed_values = self
-            .group()
-            .cayley_table()
-            .ok_or(TransformationError("Cayley table not found in the symmetry group associated with the PES specification.".to_string()))
-            .and_then(|ctb| {
-                ctb
-                    .slice(s![.., gi])
-                    .iter()
-                    .position(|&x| x == 0)
-                    .ok_or(TransformationError(
-                        format!("Unable to find the inverse of group element `{gi}`.")
-                    )).map(|giinv| {
-                        let giinv_perm = ctb.slice(s![giinv, ..]).iter().cloned().collect_vec();
-                        self.values().select(Axis(0), &giinv_perm)
-                    })
-            })?;
-        self.values = transformed_values;
-        if symop.contains_time_reversal() {
-            self.transform_cc_mut();
-        }
-        Ok(self)
-    }
-
-    /// Performs a coupled spin-spatial transformation according to a specified symmetry operation
-    /// in-place.
-    ///
-    /// Note that only $`\mathsf{SU}(2)`$ rotations can effect spin transformations.
-    ///
-    /// This is a specialised implementation for [`PES`] since the default implementation of this
-    /// method for the [`SymmetryTransformable`] trait makes use of [`Self::transform_spatial_mut`]
-    /// which is inefficient for [`PES`].
-    ///
-    /// # Arguments
-    ///
-    /// * `symop` - A symmetry operation.
-    fn sym_transform_spin_spatial_mut(
-        &mut self,
-        symop: &SymmetryOperation,
-    ) -> Result<&mut Self, TransformationError> {
-        // PESes are entirely spatial, so spin transformations have no effects.
-        self.sym_transform_spatial_mut(symop)
     }
 }
